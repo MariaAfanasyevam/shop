4777bc5cd7f4389b313f47189d0af04d2f8ae661
MariaAfanasievam <afanmaria@mail.ru>
2025-07-23 14:40:33 +0300

    fix: update SCSS structure and remove unused stores

--- old/menu.js
+++
@@ -1,9 +0,0 @@
-const burger = document.getElementById('burger');
-const menu = document.getElementById('mobile-menu');
-
-export function handleCloseMenu() {
-    burger.addEventListener('click', () => {
-        menu.classList.toggle('hidden');
-        burger.classList.toggle('active');
-    });
-}
